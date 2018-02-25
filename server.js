50eef502b03bdd203f829495a4df0d75c50f3ef1
Glitch (ivory-ounce) <none>
2018-02-25 04:27:27 +0000

    ⚡️🎳 Checkpoint server.js:659935/92 views/index.html:659935/1

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get("/dreams", function (request, response) {
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
 app.post("/posters", function (request, response) {
   console.log(request.body)
+
   response.sendStatus(200);
 });
 
