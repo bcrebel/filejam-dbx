fe3a4958a087427fdd833510d56f6fe69740c69f
Glitch (ivory-ounce) <none>
2018-02-25 21:03:37 +0000

    🚣⛽️ Checkpoint dropbox.js:659935/63

--- dropbox.js
+++ dropbox.js
@@ -9,16 +9,17 @@ let upload = (body, files) => {
   let poster = fs.readFileSync(files.poster[0].path)
   console.log(files.poster[0].path)
   let dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN });
-  dbx.filesUpload({content: poster, path:`${pathToApp + body.project}/${body.name}.jpg`})
-  .then((metadata) => { console.log(metadata)})
-  .catch((error) => {console.log(error)})
-
-  // dbx.filesUploadSessionStart({content: data.poster, path: `${pathToApp}/${data.project}/poster1.jpg`})
-  // .then((response) => {
-  //   dbx.filesUploadSessionFinish({ cursor: { "session_id": response.session_id, "offset": 0 }, contents: data.poster })
-  //   .catch((error) => { console.log(error)})
-  // })
+  // dbx.filesUpload({content: poster, path:`${pathToApp + body.project}/${body.name}.jpg`})
+  // .then((metadata) => { console.log(metadata)})
   // .catch((error) => {console.log(error)})
+
+  return dbx.filesUploadSessionStart({content: poster, path: `${pathToApp + body.project}/${body.name}.jpg`})
+  .then((response) => {
+    console.log('here')
+    dbx.filesUploadSessionFinish({ cursor: { "session_id": response.session_id, "offset": 0 }, content: poster })
+    .catch((error) => { console.log(error)})
+  })
+  .catch((error) => {console.log(error)})
 }
 
 module.exports = { upload }
