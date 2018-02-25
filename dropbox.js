231753457e085a7cbe3a2cc212ba51cbd83c3e9f
Glitch (ivory-ounce) <none>
2018-02-25 07:13:51 +0000

    🍀🕷 Checkpoint views/index.html:659935/27 dropbox.js:659935/159

--- dropbox.js
+++ dropbox.js
@@ -4,13 +4,17 @@ let pathToApp = '/Apps/filejam/'
 
 let upload = (data) => {
   let dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN });
-
-  dbx.filesUploadSessionStart({content: data.payload, path: `${pathToApp}/${data.project}/poster1.jpg`})
-  .then((response) => {
-    dbx.filesUploadSessionFinish({ cursor: { "session_id": response.session_id, "offset": 0 }, contents: data.payload })
-    .catch((error) => { console.log(error)})
-  })
+  
+  dbx.filesUpload({content: data.poster, path:`${pathToApp}/${data.project}/poster1.jpg`})
+  .then((metadata) => { console.log(metadata)})
   .catch((error) => {console.log(error)})
+
+  // dbx.filesUploadSessionStart({content: data.poster, path: `${pathToApp}/${data.project}/poster1.jpg`})
+  // .then((response) => {
+  //   dbx.filesUploadSessionFinish({ cursor: { "session_id": response.session_id, "offset": 0 }, contents: data.poster })
+  //   .catch((error) => { console.log(error)})
+  // })
+  // .catch((error) => {console.log(error)})
 }
 
 module.exports = { upload }
