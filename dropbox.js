db1f9e82cab8f1d03de9a6b5393cfc29f4895aea
Glitch (ivory-ounce) <none>
2018-02-25 06:53:19 +0000

    ⏲🛤 Checkpoint dropbox.js:659935/140 views/index.html:659935/2

--- dropbox.js
+++ dropbox.js
@@ -6,15 +6,11 @@ let upload = (data) => {
   let dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN });
 
   dbx.filesUploadSessionStart({content: data.payload, path: `${pathToApp}/${data.project}/poster1.jpg`})
-  .then((response) =>
-    dbx.filesUploadSessionFinish({
-      cursor:  {
-        "session_id": response.session_id,
-        "offset": 0
-      },
-      contents: data.payload
-    })
-    .catch((error) => {console.log(error)})
+  .then((response) => {
+    dbx.filesUploadSessionFinish({ cursor: { "session_id": response.session_id, "offset": 0 }, contents: data.payload })
+    .catch((error) => { console.log(error)})
+  })
+  .catch((error) => {console.log(error)})
 }
 
 module.exports = { upload }
