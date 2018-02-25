08905612259c070c7cdc986c3ac95ed8e4740e2f
Glitch (ivory-ounce) <none>
2018-02-25 16:25:37 +0000

    🍫👰 Checkpoint server.js:659935/47

--- server.js
+++ server.js
@@ -21,10 +21,9 @@ app.get("/", function (request, response) {
 });
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/posters", load.fields([]), function (request, response) {
+app.post("/posters", load.fields([{ name: 'poster', maxCount: 1 }, { name: 'project' }]), function (request, response) {
   // console.log(request.body)
-  console.log(request.body)
-  // upload(request.body)
+  upload(request.body)
   response.sendStatus(200);
 });
 
