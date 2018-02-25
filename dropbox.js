8b03bc21afdeedec61aeef95beab6b0f9f1a163a
Glitch (ivory-ounce) <none>
2018-02-25 06:01:09 +0000

    🌗👢 Checkpoint dropbox.js:659935/242 .env:659935/2 views/index.html:659935/7

--- dropbox.js
+++ dropbox.js
@@ -1,8 +1,12 @@
 require('isomorphic-fetch');
 const Dropbox = require('dropbox').Dropbox;
 
-let upload = () => {
-  console.log('worked')
+let upload = (data) => {
+  let dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN });
+  dbx.filesUpload({content: data.payload, path: `${new/data.project}/poster1.jpg`})
+  .then((metadata) => { 
+    console.log(metadata)
+  })
 }
 
 module.exports = { upload }
