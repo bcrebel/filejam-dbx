fc8825a727afe7fb899888eef1f7afa32db1915b
Glitch (ivory-ounce) <none>
2018-02-26 15:33:08 +0000

    💏🌗 Checkpoint dropbox.js:659935/40

--- dropbox.js
+++ dropbox.js
@@ -8,68 +8,6 @@ let targetFiles = []
 let populate = () => {
   let fakeProjects = [ "Wonder Woman\'s Gal Gadot", "Instagram Illusions" ]
   
-//   function isFolder(file) {
-//     let dateRegex = /[a-zA-Z]{3,9}\s+\d{1,2}/g
-//     let posterRegex = /poster/ig
-//     // console.log(file)
-//     if( file['.tag'] != 'folder' ) {
-//       return false
-//     } else if(dateRegex.test(file.name) | posterRegex.test(file.name)) {
-//       return false
-//     } else {
-//       return true
-//     }
-//   }
-  
-//   let getFiles = (path, condition) => {
-//     return new Promise((resolve, reject) => {
-      
-//       let start = dbx.filesListFolder({ path: path, recursive: true, limit: 5 })
-      
-//       return build()
-      
-//       function pushFiles(entries) {
-//         entries.forEach((file) => {
-//           if(condition(file)) {
-//             console.log(file.name)
-//             targetFiles.push(file.name)
-//           }
-//         })
-//       }
-      
-//       function cont(cursor) {
-//         dbx.filesListFolderContinue({cursor}).then((result) => {
-//           pushFiles(result.entries)
-//           if(result.has_more) {
-//             return cont(result.cursor)
-//           } else {
-//             resolve(targetFiles)
-//           }
-//         }).catch((error) => {
-//           console.log(error)
-//         })
-//       }
-      
-//       function build() {
-//         start.then((result) => {
-//           pushFiles(result.entries)
-          
-//           if(result.has_more) {
-//             return cont(result.cursor)
-//           } else {
-//             resolve(targetFiles)
-//           }
-//         }).catch((error) => {
-//           console.log(error)
-//         })
-//       }
-//     })
-//   } 
-  
-//   getFiles('/STAMP/ELLE/Projects', isFolder)
-//   .then((files) => {
-//     console.log(files)
-//   })
   return fakeProjects
 }
 
@@ -79,6 +17,9 @@ let upload = (body, files) => {
     send(data)
     .then((metadata) => {
       console.log(metadata)
+      
+      // Delete local file
+      fs.unlinkSync(poster)
     })
     .catch((error) => {
       console.log(error)
@@ -86,8 +27,7 @@ let upload = (body, files) => {
   })
   
   function send(data) {
-    console.log(`${pathToApp + body.brand}/${body.project}/${body.name.replace('mp4','jpg')}`)
-    return dbx.filesUpload({contents: data, path:`${pathToApp + body.brand}/${body.project}/${body.name.replace('mp4','jpg')}`, mode: 'overwrite'}) 
+    return dbx.filesUpload({contents: data, path:`${pathToApp + body.brand}/${body.project}/${body.name.replace('mp4','jpg')}`, mode: 'overwrite'})
   }
 }
 
