56412aa4f33ec7cb04f41a5bbcf803a001543455
Glitch (ivory-ounce) <none>
2018-02-26 04:20:36 +0000

    🚀⛽️ Checkpoint dropbox.js:659935/205

--- dropbox.js
+++ dropbox.js
@@ -2,11 +2,55 @@ require('isomorphic-fetch');
 const Dropbox = require('dropbox').Dropbox;
 let pathToApp = '/Apps/filejam/'
 let fs = require('fs')
-
+let dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN });
+let targetFiles = []
 
 let populate = () => {
   let fakeProjects = [ "Wonder Woman\'s Gal Gadot", "Instagram Illusions" ]
-  return fakeProjects
+  
+  let getFiles = (path, condition) => {
+    return new Promise((resolve, reject) => {
+      
+      let start = dbx.filesListFolder({ path: path, recursive: true, include_media_info: true, limit: 5 })
+      
+      return build()
+      
+      function pushFiles(entries) {
+        entries.forEach((file) => {
+          if(condition(file)) {
+            targetFiles.push(file)
+          }
+        })
+      }
+      
+      function cont(cursor) {
+        dbx.filesListFolderContinue({cursor}).then((result) => {
+          pushFiles(result.entries)
+          if(result.has_more) {
+            return cont(result.cursor)
+          } else {
+            resolve(targetFiles)
+          }
+        }).catch((error) => {
+          console.log(error)
+        })
+      }
+      
+      function build() {
+        start.then((result) => {
+          pushFiles(result.entries)
+          
+          if(result.has_more) {
+            return cont(result.cursor)
+          } else {
+            resolve(targetFiles)
+          }
+        }).catch((error) => {
+          console.log(error)
+        })
+      }
+    })
+  }  
 }
 
 let upload = (body, files) => {
@@ -22,7 +66,6 @@ let upload = (body, files) => {
   })
   
   function send(data) {
-    let dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN });
     console.log(`${pathToApp + body.brand}/${body.project}/${body.name.replace('mp4','jpg')}`)
     return dbx.filesUpload({contents: data, path:`${pathToApp + body.brand}/${body.project}/${body.name.replace('mp4','jpg')}`, mode: 'overwrite'}) 
   }
