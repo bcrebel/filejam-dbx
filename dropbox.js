5b7e7f2b7478f385a76a02fd030f633e8dd11f63
Glitch (ivory-ounce) <none>
2018-02-27 16:35:21 +0000

    👦🚇 Checkpoint dropbox.js:659935/113

--- dropbox.js
+++ dropbox.js
@@ -22,15 +22,7 @@ let upload = (body, files) => {
   
   function process() {
       files.canvasImage.forEach((image, idx, arr) => {
-        console.log('idx')
-        console.log(idx)
-        console.log('arr.length')
-        console.log(arr.length)
         doit(image, idx, arr)
-        // if(idx === arr.length - 1) { 
-        //   console.log('should be final')
-        //   console.log(util.inspect(feed, { showHidden: true, depth: null }))
-        // }
       })
   }
 
@@ -43,61 +35,38 @@ let upload = (body, files) => {
 
     fs.readFile(poster, (err, data) => {
       send(data, image.originalname)
+     
       .then((posterMetadata) => {
-        createLink(posterMetadata.path_lower)
+      if(posterMetadata == undefined) { 
+        // console.log('no poster made')
+        return doit(image, idx)  
+      } else if (posterMetadata) {
+          createLink(posterMetadata.path_lower)
+
         .then((linkMetadata) => {
-          let video = { video: {} }
-          let poster = { poster: {} }
-          video['video'][image.originalname] = body.videoLink[idx]
-          poster['poster'][posterMetadata.name] = linkMetadata.url.replace('dl=0', 'dl=1')
-          feed[body.brand][body.project]["slides"].push({})
-          let slide = Object.assign({}, video, poster)
-          console.log('video')
-          console.log(video)
-          feed[body.brand][body.project]["slides"][idx] = slide;
-          
-          console.log(util.inspect(feed, { showHidden: true, depth: null }))
-          
-          // fs.unlinkSync(poster)
+          if(linkMetadata) {
+            let video = { video: {} }
+            let poster = { poster: {} }
+            video['video'][image.originalname] = body.videoLink[idx]
+            poster['poster'][posterMetadata.name] = linkMetadata.url.replace('dl=0', 'dl=1')
+            feed[body.brand][body.project]["slides"].push({})
+            let slide = Object.assign({}, video, poster)
+            console.log('video')
+            console.log(video)
 
+            feed[body.brand][body.project]["slides"][idx] = slide;
+            _.remove(feed[body.brand][body.project]["slides"], _.isEmpty)
+
+            console.log(util.inspect(feed, { showHidden: true, depth: null }))
+          }
         })
-      }, (reason) => {
-        console.log('reason')
-        console.log(reason)
-      })
-      
-//         .then((posterMetadata) => {
-//         if(posterMetadata == undefined) { 
-//           // console.log('no poster made')
-//           return doit(image, idx)  
-//         } else if (posterMetadata) {
-//             createLink(posterMetadata.path_lower)
 
-//           .then((linkMetadata) => {
-//             if(linkMetadata) {
-//               let video = { video: {} }
-//               let poster = { poster: {} }
-//               video['video'][image.originalname] = body.videoLink[idx]
-//               poster['poster'][posterMetadata.name] = linkMetadata.url.replace('dl=0', 'dl=1')
-//               feed[body.brand][body.project]["slides"].push({})
-//               let slide = Object.assign({}, video, poster)
-//               console.log('video')
-//               console.log(video)
-
-//               feed[body.brand][body.project]["slides"][idx] = slide;
-//               _.remove(feed[body.brand][body.project]["slides"], _.isEmpty)
-
-//               console.log(util.inspect(feed, { showHidden: true, depth: null }))
-//             }
-//           })
-
-
-          // Add link to video to feed
           // Delete local file
-          // fs.unlinkSync(poster)
-        // }
-      // })
+          fs.unlinkSync(poster)
+        }
+      })
       .catch((error) => {
+        console.log('lower error')
         console.log(error)     
       })
       
@@ -108,9 +77,10 @@ let upload = (body, files) => {
   function send(data, name) {
     return dbx.filesUpload({contents: data, path:`${pathToApp + body.brand}/${body.project}/${name.replace('mp4','jpg')}`, mode: 'overwrite'})
     .catch((error) => {
-      // console.log(error)
+      console.log(error)
       if (error.status == 429) {
-        setTimeout(function() { return doit(data, name) }, 300000);
+        console.log('from error.status')
+        setTimeout(function() { return send(data, name) }, 300000);
       }          
     })
   }
